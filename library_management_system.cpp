b33ec8ab2722065dc5c2b23774dd2e325b20d292
l-kaushik <contact.lokeshkaushik@gmail.com>
2024-01-22 15:02:51 +0530

    Book DS created 1. Added Getters and Setters for Book DS

--- library_management_system.cpp
+++ library_management_system.cpp
@@ -5,8 +5,48 @@ Author:- Lokesh Kaushik
 */
 
 #include <iostream>
+#include <string>
+#include <string_view>
+
+// Book Data Structure
+class Book{
+public:
+
+//constructor
+
+//getters
+std::string_view getTitle() {return m_title;}
+std::string_view getAuthor() {return m_author;}
+std::string_view getISBN() {return m_ISBN;}
+bool getAvailablityStatus() {return m_availabilityStatus;}
+
+//setters
+void setTitle(std::string_view title) {m_title = title;} 
+void setAuthor(std::string_view author) {m_author = author;} 
+void setISBN(std::string_view ISBN) {m_ISBN = ISBN;} 
+void setAvailablityStatus(bool status) {m_availabilityStatus = status;} 
+
+private:
+    std::string m_title{};
+    std::string m_author{};
+    std::string m_ISBN{};
+    bool m_availabilityStatus {};
+};
+
 
 int main()
 {
+    Book english;
+
+    english.setTitle("10 ways to learn efficient english.");
+    english.setAuthor("Leonardo Caprio.");
+    english.setISBN("34534535363");
+    english.setAvailablityStatus(true);
+
+    std::cout<<english.getTitle()<<std::endl;
+    std::cout<<english.getAuthor()<<std::endl;
+    std::cout<<english.getAvailablityStatus()<<std::endl;
+    std::cout<<english.getISBN()<<std::endl;
+
     return 0;
 }
